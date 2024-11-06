6e4e198e23e6a244cc945769096c6824a3174254
notbattle <migualcantara@gmail.com>
2024-11-05 18:32:43 -0800

    Update White.js

--- White.js
+++ White.js
@@ -1,40 +1,56 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension) {
     var API = new APIConstructor();
-
-	return iatExtension({
-		category1 : {
-			name : 'Words A-M', //Will appear in the data.
-			title : {
-				media : {word : 'A-M'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		  {word : 'Emma'}, 
-    			{word : 'Liam'},  
-    			{word : 'Elijah'}, 
-    			{word : 'Mia'},
-          {word : 'James'},
-          {word : 'Ellie'},
-          {word : 'Aria'},
-          {word : 'Ethan'},
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'N-Z', //Will appear in the data.
-			title : {
-				media : {word : 'N-Z'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		  {word : 'Olivia'}, 
-    			{word : 'Noah'}, 		], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		} 
-	});
+    function iatExtension(options) {
+        var API = new APIConstructor();
+        var scorer = new Scorer();
+        var piCurrent = API.getCurrent();
+        var iatObj = {
+            isTouch: true,
+            canvas: {
+                maxWidth: 725,
+                proportions: 0.7,
+                background: '#ffffff',
+                borderWidth: 5,
+                canvasBackground: '#ffffff',
+                borderColor: 'lightblue',
+                css: { "touch-action": "manipulation" }
+            },
+            return: iatExtension({
+                category1: {
+                    name: 'Words A-M',
+                    title: {
+                        media: { word: 'A-M' },
+                        css: { color: '#31940F', 'font-size': '2em' },
+                        height: 4 //Used to position the "Or" in the combined block.
+                    },
+                    stimulusMedia: [
+                        { word: 'Emma' },
+                        { word: 'Liam' },
+                        { word: 'Elijah' },
+                        { word: 'Mia' },
+                        { word: 'James' },
+                        { word: 'Ellie' },
+                        { word: 'Aria' },
+                        { word: 'Ethan' },
+                    ],
+                    //Stimulus css (style)
+                    stimulusCss: { color: '#31940F', 'font-size': '1.8em' }
+                },
+                category2: {
+                    name: 'N-Z',
+                    title: {
+                        media: { word: 'N-Z' },
+                        css: { color: '#31940F', 'font-size': '2em' },
+                        height: 4 //Used to position the "Or" in the combined block.
+                    },
+                    stimulusMedia: [
+                        { word: 'Olivia' },
+                        { word: 'Noah' },
+                    ],
+                    //Stimulus css
+                    stimulusCss: { color: '#31940F', 'font-size': '1.8em' }
+                },
+            })
+        };
+    }
 });
